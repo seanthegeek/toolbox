116a32e4c1b3c44b7f249c24f2483a106132e299
Sean Whalen <whalenster@gmail.com>
2016-07-12 12:34:06 -0400

    Add comment

--- search-dump/search-dump.sh
+++ search-dump/search-dump.sh
@@ -2,7 +2,7 @@
 
 tmpfile=$(mktemp /tmp/dumpsearch.XXXXXX)
 
-cat mydomains.csv | tr -d "\r" > $tmpfile
+cat mydomains.csv | tr -d "\r" > $tmpfile # Remove Windows line endings
 
 fgrep -iR -f $tmpfile --include "*.txt" --include "*.TXT" | sort | uniq
 
